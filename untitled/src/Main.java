0f951ba4b3889e810d3eaed5e40478e1ff9c472c
bertaarenas <berta.arenas2007@gmail.com>
2025-11-05 09:21:22 +0100

    cambio1

---
+++ untitled/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Menu programa
+        System.out.println("Bienvenido al menu, a continuacion verás 5 opciones de programa.");
+        System.out.println("1) Calculadora.");
+        System.out.println("2) Calculador de edad.");
+        System.out.println("3) Conversor de temperatura.");
+        System.out.println("4) Contador de Palabras i Caracteres.");
+        System.out.println("5) Generador de saludos.");
+        System.out.println("Introduce una opción del 1 al 5.");
+
+        int opcion = sc.nextInt();
+
+        System.out.println("Has escogido la opción " + opcion+".");
+
+        if (opcion == 1) {
+
+        }
+        else if (opcion == 2) {
+
+        }
+        else if (opcion == 3) {
+
+        }
+        else if (opcion == 4) {
+
+        }
+        else if (opcion == 5) {
+
+        }
+        else {
+            System.out.println("Has escogido una opción incorrecta.");
+        }
+
+
+
+
+
+        // Calculador de edad
+
+    }
+}
+
