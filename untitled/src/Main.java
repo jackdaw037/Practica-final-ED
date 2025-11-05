8b606ba7153ae3c256301b2cea6d414268b3b5c2
jackdaw037 <santisalserin.sr@gmail.com>
2025-11-05 09:29:05 +0100

    cambio cl

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -17,7 +17,36 @@ public class Main {
         System.out.println("Has escogido la opción " + opcion+".");
 
         if (opcion == 1) {
+            double resultado = 0;
 
+            System.out.print("Ingresa el primer número: ");
+            double num1 = sc.nextDouble();
+
+            System.out.print("Ingresa el operador (+, -, *, /): ");
+            char operador = sc.next().charAt(0);
+
+            System.out.print("Ingresa el segundo número a operar: ");
+            double num2 = sc.nextDouble();
+
+            if (operador == '+') {
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+            } else if (operador == '-') {
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+            } else if (operador == '*') {
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+            } else if (operador == '/') {
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Error: no se puede dividir entre cero.");
+                }
+            } else {
+                System.out.println("Operador no válido.");
+            }
         }
         else if (opcion == 2) {
 
